ca3a95992fab533d33e123565f191eb9ad84c588
lindsayw89 <lindsay.89@outlook.com>
2019-06-08 13:00:57 -0500

    changes 06082 1

---
+++ myPortfolio/personal-Testing/JQueryHtml.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+$('p').css('border','4px solid red');
+$('p').css ('fontSize', '10px');
+$('h1').css ('fontSize', '80px');
+
+
+
+
+
+document.getElementById ("head").innerHTML='work';
+head.style.borderBottom = 'solid 3px blue';
+
+});
+
+''
