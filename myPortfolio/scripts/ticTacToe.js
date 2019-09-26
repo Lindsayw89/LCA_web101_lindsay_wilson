f240d92243bf1b98778986b4fb1bd16a85a1c35e
lindsayw89 <lindsay.89@outlook.com>
2019-09-25 22:37:01 -0500

    changes 925 1036

--- myPortfolio/scripts/ticTacToe.js
+++ myPortfolio/scripts/ticTacToe.js
@@ -1,3 +1,5 @@
+
+
 var xTurn = true;
 function placemarker (ID) {
     if(xTurn){
@@ -9,3 +11,29 @@ function placemarker (ID) {
 
     xTurn = !xTurn;
 }
+
+/*
+
+function changetoX () {
+    gamemarker='x'
+    console.log("The x button was clicked!")
+}
+
+function changetoO(){
+gamemarker='o';
+console.log("The o button was clicked!")
+
+}
+function placemarker(ID) {
+    if(ID==='10') {
+        document.getElementById(ID).innerHTML='x';}
+        else{
+            document.getElementById(ID).innerHTML='o'; 
+        }
+
+
+    }
+    */
+
+//if changeto o==id 11 then innerhtml='x'
+
